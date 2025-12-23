6f06eec3a46d71a9c87118df8d874af470e65fd7
Jimmy549 <ehmitservices1@gmail.com>
2025-12-23 14:40:37 +0500

    Update CORS for production

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:8000',
+    origin: process.env.FRONTEND_URL || ['http://localhost:8000', 'https://frontend-ashen-gamma-48.vercel.app'],
     methods: ['GET', 'POST'],
   });
   const port = process.env.PORT || 3000;
